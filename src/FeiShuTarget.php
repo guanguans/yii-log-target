05fb682f7a1199d5ea8d67cd01f48da692f9504b
yaozm <ityaozm@gmail.com>
2021-05-16 18:17:45 +0800

    refactor: Refactor FeiShuTarget

---
+++ src/FeiShuTarget.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * This file is part of the guanguans/yii-log-target.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Guanguans\YiiLogTarget;
+
+use Guanguans\Notify\Clients\BarkClient;
+use Guanguans\Notify\Clients\DingTalkClient;
+use Guanguans\Notify\Messages\FeiShu\TextMessage;
+use Yii;
+
+class FeiShuTarget extends Target
+{
+    /**
+     * @var BarkClient
+     */
+    protected $client;
+
+    /**
+     * @var \Guanguans\Notify\Messages\Chanify\TextMessage
+     */
+    protected $message;
+
+    /**
+     * @var string
+     */
+    public $keyword;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        $this->message = Yii::createObject(TextMessage::class);
+        $this->message->setOptions($this->messageOptions);
+        $this->message->setOption('text', sprintf("%s\n%s", $this->keyword, $this->getLogContext()));
+
+        $this->client = Yii::createObject(DingTalkClient::class);
+        $this->client->setToken($this->token);
+        $this->secret && $this->client->setSecret($this->secret);
+        $this->client->setMessage($this->message);
+    }
+}
